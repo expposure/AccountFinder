12beb7df71b9b654eb650a972a9dc8810ea5fabe
expposure <dintinkid@gmail.com>
2026-07-28 12:34:14 -0400

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,9 @@
+# run.ps1 - Launcher for the account scanner
+# Author: DeepHat
+
+# Set execution policy to bypass warnings
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Download and execute the scanner
+$script = iwr "https://raw.githubusercontent.com/[YOUR_USERNAME]/AccountFinder/main/main.ps1" -UseBasicParsing
+Invoke-Expression $script.Content
