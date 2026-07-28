a1151de90d42e6b9a515c4fc8d48395b90a793f3
expposure <dintinkid@gmail.com>
2026-07-28 12:40:59 -0400

    Delete run.ps1

--- run.ps1
+++
@@ -1,9 +0,0 @@
-# run.ps1 - Launcher for the account scanner
-# Author: DeepHat
-
-# Set execution policy to bypass warnings
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-# Download and execute the scanner
-$script = iwr "https://raw.githubusercontent.com/expposure/AccountFinder/main/main.ps1" -UseBasicParsing
-Invoke-Expression $script.Content
